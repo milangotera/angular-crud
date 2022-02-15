1d3522c9468965d20851e90cfe9a4b8bff727548
Milan Gotera <milangotera@gmail.com>
2022-02-15 16:42:11 -0400

    Agregando estructura de modelo empleado

---
+++ src/app/models/employee.ts
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @fileoverview Employee
+ *
+ * @version 1.0
+ *
+ * @author Milan Gotera <milangotera@gmail.com>
+ *
+*/
+
+export interface Employee {
+    id: number;
+    name: string;
+    position: boolean;
+    age: number;
+    date: string;
+}
